d30e3f2190bb03f84aba3985e54385d092260621
Ziapa <SmiT4eggg@bk.ru>
2022-04-26 16:52:31 +0300

    add users, cross-env

--- src/app,module.ts
+++ src/app,module.ts
@@ -1,11 +1,27 @@
-import {Module} from "@nestjs/common";
-import {AppController} from "./app.controller";
-import {AppServices} from "./app.services";
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { ConfigModule } from "@nestjs/config";
+import { User } from "./users/user.model";
 
 
 @Module({
-    controllers: [AppController],
-    providers:[AppServices]
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`
+    }),
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      host: process.env.POSTGRESS_HOST,
+      port: Number(process.env.POSTGRESS_PORT) ,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRESS_DB,
+      models: [User],
+      autoLoadModels: true
+    })
+  ]
 })
 export class AppModule {
 }
--- src/app.services.ts
+++
@@ -1,9 +0,0 @@
-import {Injectable} from "@nestjs/common";
-
-
-@Injectable()
-export class AppServices {
-    getUsers() {
-        return [{id: 1, name: 'Ziapa'}]
-    }
-}
