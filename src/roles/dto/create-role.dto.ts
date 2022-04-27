cc7b1709b4ad6464c48e106dbbf96b3bdaa137c1
Ziapa <SmiT4eggg@bk.ru>
2022-04-27 14:01:43 +0300

    add Auth, Login, Registration

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,4 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateRoleDto {
+
+  @ApiProperty({ example: "ADMIN", description: "Администротор"})
   readonly value: string;
+
+  @ApiProperty({ example: "Может все!", description: "Описание роли"})
   readonly description: string;
 }
